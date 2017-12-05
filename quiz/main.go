4b0a3b9e708f959b9ffacc31e84404efd80c24e3
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-12-05 11:25:44 +0300

    Started working on first exercise.

---
+++ quiz/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+	reader := csv.NewReader(file)
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
